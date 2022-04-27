20bf20472cca9de0cbf579e93ce331c6da4266ba
yjyoon-dev <yjyoon.dev@gmail.com>
2022-04-27 21:34:35 +0900

    ISSUE-014 @HiltViewModel 추가

--- modules/presentation/src/main/java/com/yapp/ui/MainViewModel.kt
+++ modules/presentation/src/main/java/com/yapp/ui/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.yapp.ui
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(): ViewModel() {
 
     private val _name = MutableStateFlow("")
     val name = _name.asStateFlow()
