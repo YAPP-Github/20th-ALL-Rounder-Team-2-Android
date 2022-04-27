feb13270f04da99989eb0b77e09fb2847a70aedb
yjyoon-dev <yjyoon.dev@gmail.com>
2022-04-27 15:48:33 +0900

    ISSUE-014 domain 모듈 추가에 따른 샘플 코드 수정

--- modules/presentation/src/main/java/com/yapp/ui/MainViewModel.kt
+++ modules/presentation/src/main/java/com/yapp/ui/MainViewModel.kt
@@ -1,21 +1,11 @@
 package com.yapp.ui
 
 import androidx.lifecycle.ViewModel
-import com.yapp.data.repo.UserRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import javax.inject.Inject
 
-@HiltViewModel
-class MainViewModel @Inject constructor(
-    userRepository: UserRepository
-) : ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val _name = MutableStateFlow("")
     val name = _name.asStateFlow()
-
-    init {
-        _name.value = userRepository.getName()
-    }
 }
