43c864d5ef2c8bcbdabc59a473ba692a8dcd1e92
yjyoon-dev <yjyoon.dev@gmail.com>
2022-04-28 22:13:16 +0900

    ISSUE-017 local 모듈 Kotlin Library -> Android Library 변경

--- modules/local/build.gradle.kts
+++ modules/local/build.gradle.kts
@@ -1,5 +1,23 @@
 plugins {
-    kotlin("jvm")
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = Versions.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Versions.MIN_SDK
+        targetSdk = Versions.TARGET_SDK
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
