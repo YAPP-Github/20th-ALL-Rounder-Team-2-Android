dd6bdcb71470b42e35247d917b45d1f83f52639b
laco-dev <laco2951@gmail.com>
2022-04-30 22:44:35 +0900

    ISSUE-010 compose.material3 라이브러리 의존성 추가

--- modules/presentation/build.gradle.kts
+++ modules/presentation/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 
     implementation(libs.androidx.compose.ui.core)
     implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
 
     implementation(libs.androidx.lifecycle.runtime)
