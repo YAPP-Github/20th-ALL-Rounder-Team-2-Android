a35506f21bd98508dbc2842f8004300a25514737
yjyoon-dev <yjyoon.dev@gmail.com>
2022-04-30 20:37:38 +0900

    ISSUE-020 코드 컨벤션에 맞추어 if문 수정

--- modules/remote/src/main/java/com/yapp/remote/source/UserRemoteDataSourceImpl.kt
+++ modules/remote/src/main/java/com/yapp/remote/source/UserRemoteDataSourceImpl.kt
@@ -4,11 +4,14 @@ import com.yapp.data.source.UserRemoteDataSource
 import javax.inject.Inject
 
 internal class UserRemoteDataSourceImpl @Inject constructor() : UserRemoteDataSource {
-    
+
     override suspend fun getName(): Result<String> {
         val random = (0..1).random()
 
-        return if (random == 0) Result.success("Name from remote")
-        else Result.failure(IllegalArgumentException())
+        return if (random == 0) {
+            Result.success("Name from remote")
+        } else {
+            Result.failure(IllegalArgumentException())
+        }
     }
 }
