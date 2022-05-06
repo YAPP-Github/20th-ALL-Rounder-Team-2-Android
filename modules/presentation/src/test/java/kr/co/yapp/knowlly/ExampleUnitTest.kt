1bee671c1cf69f12ffe7d5f09f66c29d8553575b
laco-dev <laco2951@gmail.com>
2022-05-06 22:39:30 +0900

    ExampleJunitTest Junit5 기반으로 수정

--- modules/presentation/src/test/java/kr/co/yapp/knowlly/ExampleUnitTest.kt
+++ modules/presentation/src/test/java/kr/co/yapp/knowlly/ExampleUnitTest.kt
@@ -1,16 +1,17 @@
 package kr.co.yapp.knowlly
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        // given & when
+        val actual = 2 + 2
+
+        // then
+        val expected = 4
+        assertThat(actual).isEqualTo(expected)
     }
 }
