a517471c186fd7249808a8dec65d252120069cb4
laco-dev <laco2951@gmail.com>
2022-05-08 17:30:42 +0900

    코루틴 예외 처리를 손쉽게 하기 위한 launch 함수 추가

--- modules/presentation/src/main/java/kr/co/yapp/knowlly/ui/base/BaseViewModel.kt
+++ modules/presentation/src/main/java/kr/co/yapp/knowlly/ui/base/BaseViewModel.kt
@@ -1,5 +1,28 @@
 package kr.co.yapp.knowlly.ui.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
-abstract class BaseViewModel : ViewModel()
+abstract class BaseViewModel : ViewModel() {
+
+    protected fun launch(
+        context: CoroutineContext = EmptyCoroutineContext,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+            handleException(throwable)
+        }
+        viewModelScope.launch(context + exceptionHandler, start = start, block = block)
+    }
+
+    protected fun handleException(throwable: Throwable) {
+        // TODO: 오류 처리
+    }
+}
