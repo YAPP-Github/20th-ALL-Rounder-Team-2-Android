a4e8afd24df93c4e0ba86fab15b1ab83da40af4d
yjyoon-dev <yjyoon.dev@gmail.com>
2022-05-08 15:45:18 +0900

    ISSUE-038 splashscreen API 삭제

--- modules/presentation/src/main/java/kr/co/yapp/knowlly/ui/SplashActivity.kt
+++ modules/presentation/src/main/java/kr/co/yapp/knowlly/ui/SplashActivity.kt
@@ -1,10 +1,8 @@
 package kr.co.yapp.knowlly.ui
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
@@ -13,10 +11,6 @@ import kotlinx.coroutines.delay
 class SplashActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val splashScreen = installSplashScreen()
-            splashScreen.setKeepOnScreenCondition { true }
-        }
         super.onCreate(savedInstanceState)
         lifecycleScope.launchWhenCreated {
             delay(1500)
