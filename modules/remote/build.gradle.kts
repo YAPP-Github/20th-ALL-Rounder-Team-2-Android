3b0bb6d5fb690035517a5d8d9533d000182d0a25
laco-dev <laco2951@gmail.com>
2022-05-08 15:56:48 +0900

    힐트 의존성 추가

--- modules/remote/build.gradle.kts
+++ modules/remote/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     kotlin("jvm")
+    kotlin("kapt")
 }
 
 dependencies {
     implementation(project(Modules.DATA))
+
+    implementation(libs.hilt.core)
+    kapt(libs.hilt.compiler)
 }
