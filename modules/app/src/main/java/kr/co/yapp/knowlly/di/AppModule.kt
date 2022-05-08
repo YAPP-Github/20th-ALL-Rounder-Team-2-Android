6a591061fc3e3ed6a8f4b25f33344017e8ebd498
laco-dev <laco2951@gmail.com>
2022-05-08 17:16:20 +0900

    DEBUG 환경일 때에만 Logger가 동작하도록 개선

--- modules/app/src/main/java/kr/co/yapp/knowlly/di/AppModule.kt
+++ modules/app/src/main/java/kr/co/yapp/knowlly/di/AppModule.kt
@@ -4,7 +4,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kr.co.yapp.knowlly.BuildConfig
 import kr.co.yapp.knowlly.log.Logger
 import kr.co.yapp.knowlly.remote.api.BaseUrl
 import kr.co.yapp.knowlly.remote.api.Interceptors
@@ -20,9 +19,7 @@ internal object AppModule {
     @Provides
     fun provideInterceptors(): Interceptors {
         val logger = HttpLoggingInterceptor.Logger { message ->
-            if (BuildConfig.DEBUG) {
-                Logger.d("OkHttp", message)
-            }
+            Logger.d("OkHttp", message)
         }
         val loggingInterceptor = HttpLoggingInterceptor(logger)
         return Interceptors(loggingInterceptor)
