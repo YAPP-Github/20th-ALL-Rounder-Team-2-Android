8e04f1b833a9fec24b5ec50469423eed656177c8
laco-dev <laco2951@gmail.com>
2022-05-08 17:14:16 +0900

    앱 실행중에는 AndroidLogger로 로그를 출력하도록 함

--- modules/app/src/main/java/kr/co/yapp/knowlly/App.kt
+++ modules/app/src/main/java/kr/co/yapp/knowlly/App.kt
@@ -2,6 +2,15 @@ package kr.co.yapp.knowlly
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kr.co.yapp.knowlly.log.AndroidLogger
+import kr.co.yapp.knowlly.log.Logger
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Logger.add(AndroidLogger)
+    }
+}
