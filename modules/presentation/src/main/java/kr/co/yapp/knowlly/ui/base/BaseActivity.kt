8ec8e44293d73efcb86f42dca42e9e676fd84d53
laco-dev <laco2951@gmail.com>
2022-05-12 00:02:57 +0900

    액티비티 전체적으로 공통 로직을 처리하기 위한 BaseActivity 추가

---
+++ modules/presentation/src/main/java/kr/co/yapp/knowlly/ui/base/BaseActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.yapp.knowlly.ui.base
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import kr.co.yapp.knowlly.ui.transition.ActivityTransition
+
+abstract class BaseActivity : ComponentActivity() {
+
+    protected open val activityTransition = ActivityTransition.Push
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityTransition.overridePendingTransition(this)
+    }
+
+    override fun finish() {
+        super.finish()
+        activityTransition.overridePendingPopTransition(this)
+    }
+}
