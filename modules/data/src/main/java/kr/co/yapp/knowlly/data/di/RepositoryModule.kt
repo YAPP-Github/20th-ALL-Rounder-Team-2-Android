7fba68ff6ce35a41a8e4154006e47d6551df9351
yjyoon-dev <yjyoon.dev@gmail.com>
2022-05-17 17:04:53 +0900

    사용하지 않는 임시 repository 삭제

--- modules/data/src/main/java/kr/co/yapp/knowlly/data/di/RepositoryModule.kt
+++ modules/data/src/main/java/kr/co/yapp/knowlly/data/di/RepositoryModule.kt
@@ -5,18 +5,12 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kr.co.yapp.knowlly.data.repo.AuthRepositoryImpl
-import kr.co.yapp.knowlly.data.repo.UserRepository
-import kr.co.yapp.knowlly.data.repo.UserRepositoryImpl
 import kr.co.yapp.knowlly.domain.repo.AuthRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class RepositoryModule {
-
-    @Binds
-    @Singleton
-    abstract fun bindUserRepository(repo: UserRepositoryImpl): UserRepository
 
     @Binds
     @Singleton
