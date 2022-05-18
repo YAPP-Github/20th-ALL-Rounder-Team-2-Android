c8a8ebf4f966e2bdde46c809efec68782a548c42
laco-dev <laco2951@gmail.com>
2022-05-18 21:43:52 +0900

    사용하지 않는 코드 삭제

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/MainActivity.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/MainActivity.kt
@@ -4,35 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import kr.co.knowledgerally.ui.base.BaseActivity
 import kr.co.knowledgerally.ui.theme.KnowllyTheme
 
 class MainActivity : BaseActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KnowllyTheme {
-                val name by viewModel.name.collectAsState()
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting(name)
-                }
             }
         }
     }
@@ -43,18 +24,5 @@ class MainActivity : BaseActivity() {
             val intent = Intent(context, MainActivity::class.java)
             context.startActivity(intent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KnowllyTheme {
-        Greeting("Android")
     }
 }
