9d51353db7e92f6888dbf81db01ab808f6d58616
laco-dev <laco2951@gmail.com>
2022-05-18 21:49:54 +0900

    compose-navigation 의존성 추가

--- modules/presentation/build.gradle.kts
+++ modules/presentation/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.navigation)
 
     implementation(libs.androidx.lifecycle.runtime)
     implementation(libs.androidx.lifecycle.viewmodel)
