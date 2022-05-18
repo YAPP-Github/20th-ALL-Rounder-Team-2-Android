53d880a3758a52b5959fb3df79881d86190d06f7
laco-dev <laco2951@gmail.com>
2022-05-18 21:42:18 +0900

    4개의 탭 기본 화면 컴포저블 추가

---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/coach/CoachScreen.kt
@@ -0,0 +1,13 @@
+package kr.co.knowledgerally.ui.coach
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun CoachScreen() {
+    Box(contentAlignment = Alignment.Center) {
+        Text(text = "운영 클래스")
+    }
+}
---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/mypage/MyPageScreen.kt
@@ -0,0 +1,13 @@
+package kr.co.knowledgerally.ui.mypage
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun MyPageScreen() {
+    Box(contentAlignment = Alignment.Center) {
+        Text(text = "마이 페이지")
+    }
+}
