b44549875e8ae4a4d6028f8aca153902917e9704
yjyoon-dev <yjyoon.dev@gmail.com>
2022-05-21 21:51:26 +0900

    구글 로그인 버튼 삭제

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/login/LoginScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/login/LoginScreen.kt
@@ -2,16 +2,12 @@ package kr.co.knowledgerally.ui.login
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import kr.co.knowledgerally.model.LoginTypeModel
 import kr.co.knowledgerally.model.SocialUserInfo
-import kr.co.knowledgerally.ui.component.KnowllyFilledButton
 import kr.co.knowledgerally.ui.component.KnowllyOutlinedButton
 
 @Composable
@@ -35,17 +31,6 @@ fun LoginScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
-        KnowllyFilledButton(
-            text = "구글 로그인",
-            onClick = { }
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        KnowllyFilledButton(
-            text = "구글 로그인",
-            onClick = { },
-            enabled = false
-        )
-        Spacer(modifier = Modifier.height(8.dp))
         KnowllyOutlinedButton(
             text = "카카오 로그인",
             onClick = {
