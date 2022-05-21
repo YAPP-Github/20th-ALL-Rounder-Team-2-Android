a1243b17846deff926964134ad6def846692948a
laco-dev <laco2951@gmail.com>
2022-05-21 14:55:49 +0900

    KnowllyOutlinedButton 구현

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/component/KnowllyButton.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/component/KnowllyButton.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.co.knowledgerally.ui.theme.KnowllyTheme
@@ -42,6 +44,32 @@ fun KnowllyFilledButton(
     }
 }
 
+@Composable
+fun KnowllyOutlinedButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val colors = ButtonDefaults.outlinedButtonColors(
+        contentColor = KnowllyTheme.colors.primaryDark,
+    )
+    val border = ButtonDefaults.outlinedButtonBorder.copy(
+        brush = SolidColor(KnowllyTheme.colors.primaryDark)
+    )
+    OutlinedButton(
+        modifier = modifier.height(BUTTON_HEIGHT),
+        shape = RoundedCornerShape(BUTTON_RADIUS),
+        colors = colors,
+        border = border,
+        onClick = onClick
+    ) {
+        Text(
+            text = text,
+            style = KnowllyTheme.typography.button1,
+        )
+    }
+}
+
 @Preview("Enabled")
 @Composable
 fun KnowllyFilledButtonPreviewEnabled() {
@@ -62,6 +90,17 @@ fun KnowllyFilledButtonPreviewDisabled() {
             text = "버튼",
             onClick = { },
             enabled = false,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun KnowllyOutlinedButtonPreview() {
+    KnowllyTheme {
+        KnowllyOutlinedButton(
+            text = "버튼",
+            onClick = { },
         )
     }
 }
