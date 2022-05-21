2342a90c140dc429a82c507e7e2e84e6b91948bf
laco-dev <laco2951@gmail.com>
2022-05-21 14:48:59 +0900

    KnowllyFilledButton 구현

---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/component/KnowllyButton.kt
@@ -0,0 +1,67 @@
+package kr.co.knowledgerally.ui.component
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.co.knowledgerally.ui.theme.KnowllyTheme
+
+private val BUTTON_RADIUS = 10.dp
+private val BUTTON_HEIGHT = 56.dp
+
+@Composable
+fun KnowllyFilledButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    val buttonColors = ButtonDefaults.buttonColors(
+        containerColor = KnowllyTheme.colors.primaryDark,
+        contentColor = KnowllyTheme.colors.grayFF,
+        disabledContainerColor = KnowllyTheme.colors.grayDD,
+        disabledContentColor = KnowllyTheme.colors.grayFF
+    )
+
+    Button(
+        modifier = modifier.height(BUTTON_HEIGHT),
+        shape = RoundedCornerShape(BUTTON_RADIUS),
+        onClick = onClick,
+        enabled = enabled,
+        colors = buttonColors
+    ) {
+        Text(
+            text = text,
+            style = KnowllyTheme.typography.button1,
+        )
+    }
+}
+
+@Preview("Enabled")
+@Composable
+fun KnowllyFilledButtonPreviewEnabled() {
+    KnowllyTheme {
+        KnowllyFilledButton(
+            text = "버튼",
+            onClick = { },
+            enabled = true,
+        )
+    }
+}
+
+@Preview("Disabled")
+@Composable
+fun KnowllyFilledButtonPreviewDisabled() {
+    KnowllyTheme {
+        KnowllyFilledButton(
+            text = "버튼",
+            onClick = { },
+            enabled = false,
+        )
+    }
+}
