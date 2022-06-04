8eabf9bb4c06ec92bd061a453d65816c00fb7e47
laco-dev <laco2951@gmail.com>
2022-06-04 16:19:25 +0900

    마이 페이지 UI 컴포저블 분리

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/mypage/MyPageScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/mypage/MyPageScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,7 +22,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -33,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -43,7 +44,6 @@ import kr.co.knowledgerally.ui.component.KnowllyContainedButton
 import kr.co.knowledgerally.ui.theme.KnowllyTheme
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
 fun MyPageScreen(viewModel: MyPageViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsState()
 
@@ -75,7 +75,7 @@ private fun MyPageScreen(
             navigateToNotification = { },
             versionName = "",
             userName = "",
-            remainingBallCount = 0,
+            ballCount = 0,
             isCoach = false,
             navigateToProfile = { },
             navigateToTermsOfService = { },
@@ -88,7 +88,7 @@ private fun MyPageScreen(
             navigateToNotification = navigateToNotification,
             versionName = state.versionName,
             userName = state.userName,
-            remainingBallCount = state.remainingBallCount,
+            ballCount = state.remainingBallCount,
             isCoach = state.isCoach,
             navigateToProfile = navigateToProfile,
             navigateToTermsOfService = navigateToTermsOfService,
@@ -105,7 +105,7 @@ private fun MyPageScreen(
     navigateToNotification: () -> Unit,
     versionName: String,
     userName: String,
-    remainingBallCount: Int,
+    ballCount: Int,
     isCoach: Boolean,
     navigateToProfile: () -> Unit,
     navigateToTermsOfService: () -> Unit,
@@ -117,220 +117,234 @@ private fun MyPageScreen(
             .fillMaxSize()
             .background(Color.White)
     ) {
-        // TopAppBar
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(56.dp),
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Surface(
-                shape = RoundedCornerShape(36.dp),
-                border = BorderStroke(
-                    width = 2.dp,
-                    color = KnowllyTheme.colors.grayEF
-                ),
-                color = Color.Unspecified,
-                modifier = Modifier
-                    .height(28.dp)
-                    .width(IntrinsicSize.Min)
-                    .clip(RoundedCornerShape(36.dp))
-            ) {
-                Row(
-                    modifier = Modifier.padding(horizontal = 12.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        modifier = Modifier.size(20.dp),
-                        painter = painterResource(id = R.drawable.ic_ball),
-                        tint = Color.Unspecified,
-                        contentDescription = null
-                    )
-                    HorizontalSpacer(width = 4.dp)
-                    Text(
-                        text = remainingBallCount.toString(),
-                        style = KnowllyTheme.typography.subtitle4
-                    )
-                }
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(2.dp)
-                        .clickable { },
+        MyPageAppBar(
+            ballCount = ballCount,
+            navigateToNotification = navigateToNotification
+        )
+        MyPageProfile(
+            userName = userName,
+            isCoach = isCoach,
+            navigateToProfile = navigateToProfile,
+        )
+        MyPageDivider()
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_allow_notification),
+            onClick = { navigateToTermsOfService() },
+            content = {
+                MyPageSwitch(
+                    checked = notificationEnabled,
+                    onCheckedChange = onNotificationEnabledChange
                 )
             }
-            Icon(
-                painter = painterResource(id = R.drawable.ic_alarm),
-                contentDescription = null,
-                tint = KnowllyTheme.colors.gray00,
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 24.dp)
-                    .size(32.dp)
-                    .clip(CircleShape)
-                    .clickable { navigateToNotification() }
-                    .padding(4.dp)
-            )
-        }
-
-        // Profile Content
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(
-                    top = 12.dp,
-                    bottom = 40.dp,
-                    start = 24.dp
-                )
-        ) {
-            Image(
-                painter = painterResource(R.drawable.img_avatar),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(end = 16.dp)
-                    .size(60.dp)
-                    .clip(CircleShape)
-            )
-
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(text = userName, style = KnowllyTheme.typography.subtitle1)
-
-                Row(modifier = Modifier.padding(top = 4.dp)) {
-                    ContainedBadge(
-                        text = "플레이어",
-                        contentColor = KnowllyTheme.colors.primaryDark,
-                        backgroundColor = KnowllyTheme.colors.primary.copy(alpha = 0.1f)
-                    )
-                    if (isCoach) {
-                        ContainedBadge(
-                            text = "코치",
-                            contentColor = KnowllyTheme.colors.secondaryLimeDark,
-                            backgroundColor = KnowllyTheme.colors.secondary.copy(alpha = 0.2f),
-                            modifier = Modifier.padding(start = 4.dp)
-                        )
-                    }
-                }
-
-                KnowllyContainedButton(
-                    text = "프로필 보기 / 수정",
-                    onClick = navigateToProfile,
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .height(40.dp)
+        )
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_app_version_info),
+            onClick = { navigateToTermsOfService() },
+            content = {
+                Text(
+                    text = versionName,
+                    style = KnowllyTheme.typography.body1,
                 )
             }
-        }
-
-        Divider(
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .height(1.dp),
-            color = KnowllyTheme.colors.grayEF
         )
-
-        Row(
-            modifier = Modifier.padding(vertical = 20.dp, horizontal = 24.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                text = "알림 허용",
-                style = KnowllyTheme.typography.subtitle4,
-                modifier = Modifier.weight(1f)
-            )
-            Switch(
-                modifier = Modifier.size(32.dp, 20.dp),
-                checked = notificationEnabled,
-                onCheckedChange = onNotificationEnabledChange,
-                colors = SwitchDefaults.colors(
-                    checkedThumbColor = KnowllyTheme.colors.primaryDark,
-                    checkedTrackAlpha = 0.38f,
-                    uncheckedThumbColor = KnowllyTheme.colors.primaryLight,
-                    uncheckedTrackColor = KnowllyTheme.colors.primaryLight,
-                    uncheckedTrackAlpha = 0.38f
-                )
-            )
-        }
-
-        Divider(
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .height(1.dp),
-            color = KnowllyTheme.colors.grayEF
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_terms_of_service_and_policy),
+            onClick = { navigateToTermsOfService() },
+            content = { MyPageIcon() }
         )
-
-        Row(modifier = Modifier.padding(vertical = 20.dp, horizontal = 24.dp)) {
-            Text(
-                text = "앱 버전 정보",
-                style = KnowllyTheme.typography.subtitle4,
-                modifier = Modifier.weight(1f)
-            )
-            Text(
-                text = versionName,
-                style = KnowllyTheme.typography.body1,
-            )
-        }
-        Divider(
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .height(1.dp),
-            color = KnowllyTheme.colors.grayEF
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_logout),
+            onClick = { logout() },
+            content = { MyPageIcon() }
         )
-
-        Row(modifier = Modifier
-            .clickable { navigateToTermsOfService() }
-            .padding(vertical = 20.dp, horizontal = 24.dp)
-        ) {
-            Text(
-                text = "서비스 이용약관 및 정책",
-                style = KnowllyTheme.typography.subtitle4,
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Divider(
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .height(1.dp),
-            color = KnowllyTheme.colors.grayEF
-        )
-
-        Row(modifier = Modifier
-            .clickable { logout() }
-            .padding(vertical = 20.dp, horizontal = 24.dp)
-        ) {
-            Text(
-                text = "로그아웃",
-                style = KnowllyTheme.typography.subtitle4,
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Divider(
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .height(1.dp),
-            color = KnowllyTheme.colors.grayEF
-        )
-
-        Row(modifier = Modifier
-            .clickable { withdrawal() }
-            .padding(vertical = 20.dp, horizontal = 24.dp)
-        ) {
-            Text(
-                text = "회원 탈퇴",
-                style = KnowllyTheme.typography.subtitle4,
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Divider(
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .height(1.dp),
-            color = KnowllyTheme.colors.grayEF
+        MyPageItem(
+            text = stringResource(id = R.string.mypage_withdrawal),
+            onClick = { withdrawal() },
+            content = { MyPageIcon() }
         )
     }
+}
+
+@Composable
+private fun MyPageAppBar(
+    ballCount: Int,
+    navigateToNotification: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        horizontalArrangement = Arrangement.End,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Surface(
+            shape = RoundedCornerShape(36.dp),
+            border = BorderStroke(
+                width = 2.dp,
+                color = KnowllyTheme.colors.grayEF
+            ),
+            color = Color.Unspecified,
+            modifier = Modifier
+                .height(28.dp)
+                .width(IntrinsicSize.Min)
+                .clip(RoundedCornerShape(36.dp))
+        ) {
+            Row(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.size(20.dp),
+                    painter = painterResource(id = R.drawable.ic_ball),
+                    tint = Color.Unspecified,
+                    contentDescription = null
+                )
+                HorizontalSpacer(width = 4.dp)
+                Text(
+                    text = ballCount.toString(),
+                    style = KnowllyTheme.typography.subtitle4
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(2.dp)
+                    .clickable { },
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_alarm),
+            contentDescription = null,
+            tint = KnowllyTheme.colors.gray00,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 24.dp)
+                .size(32.dp)
+                .clip(CircleShape)
+                .clickable { navigateToNotification() }
+                .padding(4.dp)
+        )
+    }
+}
+
+@Composable
+private fun MyPageProfile(
+    userName: String,
+    isCoach: Boolean,
+    navigateToProfile: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(
+                top = 12.dp,
+                bottom = 40.dp,
+                start = 24.dp
+            )
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_avatar),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(end = 16.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(text = userName, style = KnowllyTheme.typography.subtitle1)
+            Row(modifier = Modifier.padding(top = 4.dp)) {
+                ContainedBadge(
+                    text = stringResource(id = R.string.player),
+                    contentColor = KnowllyTheme.colors.primaryDark,
+                    backgroundColor = KnowllyTheme.colors.primary.copy(alpha = 0.1f)
+                )
+                if (isCoach) {
+                    ContainedBadge(
+                        text = stringResource(id = R.string.coach),
+                        contentColor = KnowllyTheme.colors.secondaryLimeDark,
+                        backgroundColor = KnowllyTheme.colors.secondary.copy(alpha = 0.2f),
+                        modifier = Modifier.padding(start = 4.dp)
+                    )
+                }
+            }
+            KnowllyContainedButton(
+                text = stringResource(id = R.string.mypage_show_more_profile),
+                onClick = navigateToProfile,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .height(40.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun MyPageItem(
+    text: String,
+    content: @Composable RowScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)? = null,
+) {
+    val clickableModifier = if (onClick != null) {
+        Modifier.clickable { onClick() }
+    } else {
+        Modifier
+    }
+
+    Row(
+        modifier = modifier
+            .then(clickableModifier)
+            .padding(vertical = 20.dp, horizontal = 24.dp)
+    ) {
+        Text(
+            text = text,
+            style = KnowllyTheme.typography.subtitle4,
+            modifier = Modifier.weight(1f)
+        )
+        content()
+    }
+    MyPageDivider()
+}
+
+@Composable
+private fun MyPageIcon() {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_chevron_right),
+        contentDescription = null,
+        tint = KnowllyTheme.colors.gray00
+    )
+}
+
+@Composable
+private fun MyPageDivider() {
+    Divider(
+        modifier = Modifier
+            .padding(horizontal = 24.dp)
+            .height(1.dp),
+        color = KnowllyTheme.colors.grayEF
+    )
+}
+
+@Composable
+private fun MyPageSwitch(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Switch(
+        modifier = Modifier.size(34.dp, 20.dp),
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = KnowllyTheme.colors.primaryDark,
+            checkedTrackAlpha = 0.38f,
+            uncheckedThumbColor = KnowllyTheme.colors.grayDD,
+            uncheckedTrackColor = KnowllyTheme.colors.gray8F,
+            uncheckedTrackAlpha = 0.38f
+        )
+    )
 }
 
 @Preview(showBackground = true)
