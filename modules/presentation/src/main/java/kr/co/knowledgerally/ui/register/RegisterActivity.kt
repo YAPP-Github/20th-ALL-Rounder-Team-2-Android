5a4f9f012ca8d689cfb818baa722f9c0e4012f8b
laco-dev <laco2951@gmail.com>
2022-06-11 17:50:36 +0900

    RegisterActivity 빈 화면 추가

---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/register/RegisterActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.knowledgerally.ui.register
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import kr.co.knowledgerally.base.BaseActivity
+import kr.co.knowledgerally.ui.theme.KnowllyTheme
+
+@AndroidEntryPoint
+class RegisterActivity : BaseActivity() {
+
+    private val viewModel: RegisterViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            KnowllyTheme {
+
+            }
+        }
+    }
+}
