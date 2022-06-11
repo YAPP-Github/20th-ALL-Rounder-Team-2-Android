0b1e7a085440a88c5c5104f752ecdf082a9f0963
yjyoon-dev <yjyoon.dev@gmail.com>
2022-06-12 00:31:14 +0900

    material -> material3 사용하도록 수정

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/component/KnowllyCheckBox.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/component/KnowllyCheckBox.kt
@@ -2,11 +2,11 @@ package kr.co.knowledgerally.ui.component
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.material.Text
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
