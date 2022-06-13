7a944e82343d19bbeffb6a7bc834a7acc76b6f71
laco-dev <laco2951@gmail.com>
2022-06-13 12:21:31 +0900

    TopAppBar 디자인 개선

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/component/KnowllyTopAppBar.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/component/KnowllyTopAppBar.kt
@@ -2,7 +2,6 @@ package kr.co.knowledgerally.ui.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
@@ -11,14 +10,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +26,7 @@ private val TOP_APP_BAR_HEIGHT = 56.dp
 @Composable
 fun KnowllyTopAppBar(
     navigationType: NavigationType = NavigationType.Back,
-    onNagationClick: () -> Unit,
+    onNavigationClick: () -> Unit,
     actions: @Composable RowScope.() -> Unit = {}
 ) {
     Row(
@@ -40,12 +36,12 @@ fun KnowllyTopAppBar(
             .fillMaxWidth()
             .height(TOP_APP_BAR_HEIGHT)
             .background(color = KnowllyTheme.colors.grayFF)
-            .padding(horizontal = 24.dp, vertical = 12.dp)
+            .padding(top = 12.dp, end = 12.dp, bottom = 12.dp)
     ) {
         if (navigationType != NavigationType.None) {
             KnowllyTopAppBarNavigation(
                 navigationType = navigationType,
-                onNavigationClick = onNagationClick
+                onNavigationClick = onNavigationClick
             )
         }
         Row(
@@ -62,22 +58,17 @@ fun KnowllyTopAppBarNavigation(
     navigationType: NavigationType,
     onNavigationClick: () -> Unit
 ) {
-    val navigationIconRes =
-        if (navigationType == NavigationType.Back) {
-            R.drawable.ic_previous
-        } else {
-            R.drawable.ic_clear
-        }
-
-    Surface(
-        modifier = Modifier
-            .clip(shape = CircleShape)
-            .size(24.dp)
-            .clickable { onNavigationClick() }
-    ) {
+    IconButton(onClick = onNavigationClick, modifier = Modifier.padding(start = 12.dp)) {
         Icon(
-            painter = painterResource(id = navigationIconRes),
-            contentDescription = null
+            painter = painterResource(
+                id = if (navigationType == NavigationType.Back) {
+                    R.drawable.ic_previous
+                } else {
+                    R.drawable.ic_clear
+                }
+            ),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp)
         )
     }
 }
@@ -92,7 +83,7 @@ private fun KnowllyTopAppBarPreviewTitle() {
     KnowllyTheme {
         KnowllyTopAppBar(
             navigationType = NavigationType.None,
-            onNagationClick = {},
+            onNavigationClick = {},
             actions = {
                 Image(
                     painter = painterResource(id = R.drawable.ic_logo_topappbar),
@@ -117,7 +108,7 @@ private fun KnowllyTopAppBarPreviewNoTitle() {
     KnowllyTheme {
         KnowllyTopAppBar(
             navigationType = NavigationType.Close,
-            onNagationClick = {},
+            onNavigationClick = {},
         )
     }
 }
