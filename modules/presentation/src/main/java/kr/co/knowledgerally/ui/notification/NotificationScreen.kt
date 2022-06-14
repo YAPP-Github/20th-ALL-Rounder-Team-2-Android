2e0afdc9d1bed7fda38598ae5f383e193c7de709
yjyoon-dev <yjyoon.dev@gmail.com>
2022-06-14 23:19:57 +0900

    material -> material3 수정

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/notification/NotificationScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/notification/NotificationScreen.kt
@@ -12,12 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
