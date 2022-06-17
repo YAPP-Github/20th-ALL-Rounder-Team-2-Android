781004baf737bd46170ea8fe117f26fce84d4397
laco-dev <laco2951@gmail.com>
2022-06-17 23:26:56 +0900

    자바8 디슈가링 추가

--- modules/app/build.gradle.kts
+++ modules/app/build.gradle.kts
@@ -54,6 +54,8 @@ android {
         }
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -68,6 +70,8 @@ android {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+
     implementation(project(Modules.PRESENTATION))
     implementation(project(Modules.DOMAIN))
     implementation(project(Modules.DATA))
