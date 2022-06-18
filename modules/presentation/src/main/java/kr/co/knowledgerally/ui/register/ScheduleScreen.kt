5ef7360e27d7d99d5684f3ede2c51e32c2a1721c
laco-dev <laco2951@gmail.com>
2022-06-18 21:55:18 +0900

    일정 추가 버튼 컴포저블 분리

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/register/ScheduleScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/register/ScheduleScreen.kt
@@ -1,25 +1,20 @@
 package kr.co.knowledgerally.ui.register
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.co.knowledgerally.ui.R
+import kr.co.knowledgerally.ui.component.AddScheduleButton
 import kr.co.knowledgerally.ui.component.KnowllyContainedButton
 import kr.co.knowledgerally.ui.component.KnowllyTopAppBar
 import kr.co.knowledgerally.ui.component.PageIndicator
@@ -57,33 +52,7 @@ fun ScheduleScreen(
                 color = KnowllyTheme.colors.gray8F,
             )
 
-            Surface(
-                shape = RoundedCornerShape(10.dp),
-                onClick = navigateToSchedule,
-                modifier = modifier
-                    .padding(top = 40.dp)
-                    .fillMaxWidth()
-                    .height(52.dp),
-                color = KnowllyTheme.colors.primaryLight,
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_add),
-                        contentDescription = null,
-                        tint = KnowllyTheme.colors.primaryDark
-                    )
-                    Text(
-                        text = stringResource(id = R.string.register_schedule_add_schedule),
-                        modifier = Modifier.padding(start = 2.dp),
-                        style = KnowllyTheme.typography.button1,
-                        color = KnowllyTheme.colors.primaryDark,
-                    )
-                }
-            }
+            AddScheduleButton(navigateToSchedule, modifier.padding(top = 40.dp))
         }
 
         KnowllyContainedButton(
--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/schedule/ScheduleScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/schedule/ScheduleScreen.kt
@@ -1,25 +1,17 @@
 package kr.co.knowledgerally.ui.schedule
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import kr.co.knowledgerally.ui.R
+import kr.co.knowledgerally.ui.component.AddScheduleButton
 import kr.co.knowledgerally.ui.component.KnowllyTopAppBar
 import kr.co.knowledgerally.ui.component.NavigationType
 import kr.co.knowledgerally.ui.theme.KnowllyTheme
@@ -71,32 +63,6 @@ private fun ScheduleContent(
             color = KnowllyTheme.colors.gray8F,
         )
 
-        Surface(
-            shape = RoundedCornerShape(10.dp),
-            onClick = showDatePicker,
-            modifier = modifier
-                .padding(top = 40.dp)
-                .fillMaxWidth()
-                .height(52.dp),
-            color = KnowllyTheme.colors.primaryLight,
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_add),
-                    contentDescription = null,
-                    tint = KnowllyTheme.colors.primaryDark
-                )
-                Text(
-                    text = stringResource(id = R.string.register_schedule_add_schedule),
-                    modifier = Modifier.padding(start = 2.dp),
-                    style = KnowllyTheme.typography.button1,
-                    color = KnowllyTheme.colors.primaryDark,
-                )
-            }
-        }
+        AddScheduleButton(showDatePicker, modifier.padding(top = 40.dp))
     }
 }
