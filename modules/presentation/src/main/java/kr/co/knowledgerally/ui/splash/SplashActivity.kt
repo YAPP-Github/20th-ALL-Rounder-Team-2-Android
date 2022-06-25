62c1d71734ac6f8869abf25559bfc9c2a5ceb375
laco-dev <laco2951@gmail.com>
2022-06-25 20:53:49 +0900

    테스트 목적으로 들어간 코드 삭제

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/splash/SplashActivity.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/splash/SplashActivity.kt
@@ -25,9 +25,6 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-        ProfileActivity.startActivity(this)
-        return
         setContent()
 
         lifecycleScope.launch {
