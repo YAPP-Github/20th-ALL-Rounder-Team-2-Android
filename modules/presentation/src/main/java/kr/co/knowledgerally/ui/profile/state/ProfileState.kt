f761b055baf94d617028cf8c965449a862046425
laco-dev <laco2951@gmail.com>
2022-06-25 18:08:42 +0900

    ProfileState에 카카오톡 아이디, 포트폴리오 State 추가

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/profile/state/ProfileState.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/profile/state/ProfileState.kt
@@ -9,17 +9,27 @@ class ProfileState(
     val nameState: NameState,
     val introductionState: IntroductionState,
     val imageState: ImageState,
+    val kakaoIdState: KakaoIdState,
+    val portfolioState: PortfolioState,
 ) {
 
     val isValid: Boolean
-        get() = nameState.isValid && introductionState.isValid
+        get() = nameState.isValid && introductionState.isValid && kakaoIdState.isValid
 }
 
 @Composable
 fun rememberProfileState(
     nameState: NameState = rememberNameState(),
     introductionState: IntroductionState = IntroductionState(),
+    kakaoIdState: KakaoIdState = rememberKakaoIdState(),
+    portfolioState: PortfolioState = PortfolioState(),
     imageState: ImageState = remember { ImageState() }
 ) = remember {
-    ProfileState(nameState, introductionState, imageState)
+    ProfileState(
+        nameState = nameState,
+        introductionState = introductionState,
+        imageState = imageState,
+        kakaoIdState = kakaoIdState,
+        portfolioState = portfolioState
+    )
 }
