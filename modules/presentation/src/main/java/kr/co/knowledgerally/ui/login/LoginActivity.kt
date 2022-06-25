b2bec747631e2a6178d57a4e9276e00d2ea9f17e
yjyoon-dev <yjyoon.dev@gmail.com>
2022-06-25 20:15:48 +0900

    로그인 이후 온보딩 진행 여부에 따른 화면 이동 구현

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/login/LoginActivity.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/login/LoginActivity.kt
@@ -16,6 +16,7 @@ import kr.co.knowledgerally.base.BaseActivity
 import kr.co.knowledgerally.feature.kakao.KakaoLogin
 import kr.co.knowledgerally.ui.main.MainActivity
 import kr.co.knowledgerally.ui.policy.PolicyActivity
+import kr.co.knowledgerally.ui.profile.ProfileActivity
 import kr.co.knowledgerally.ui.signup.SignUpActivity
 import kr.co.knowledgerally.ui.terms.TermsActivity
 import kr.co.knowledgerally.ui.theme.KnowllyTheme
@@ -66,14 +67,20 @@ class LoginActivity : BaseActivity() {
         viewModel.state.collect { state ->
             when (state) {
                 is LoginState.NeedToSignUp -> startSignUpActivity(state.providerAccessToken)
-                LoginState.NotLoggedIn -> Unit // TODO
-                LoginState.Success -> Unit // TODO
+                LoginState.NeedToOnboard -> startProfileActivity()
+                LoginState.Success -> startMainActivity()
+                LoginState.NotLoggedIn -> Unit
             }
         }
     }
 
     private fun startMainActivity() {
         MainActivity.startActivity(this)
+        finish()
+    }
+
+    private fun startProfileActivity() {
+        ProfileActivity.startActivity(this)
         finish()
     }
 
--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/login/LoginViewModel.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/login/LoginViewModel.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kr.co.knowledgerally.base.BaseViewModel
 import kr.co.knowledgerally.domain.model.ProviderToken
+import kr.co.knowledgerally.domain.usecase.IsOnboardedUseCase
 import kr.co.knowledgerally.domain.usecase.IsSignedUpUseCase
 import kr.co.knowledgerally.domain.usecase.SignInUseCase
 import javax.inject.Inject
@@ -13,24 +14,24 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val isSignedUpUseCase: IsSignedUpUseCase,
-    private val signInUseCase: SignInUseCase
+    private val signInUseCase: SignInUseCase,
+    private val isOnboardedUseCase: IsOnboardedUseCase
 ) : BaseViewModel() {
 
     private val _state = MutableStateFlow<LoginState>(LoginState.NotLoggedIn)
     val state: StateFlow<LoginState> = _state.asStateFlow()
-
-    /**
-     * TODO
-     * 1. 현재 AccessToken이 가입된 유저인지 검사
-     * 2. 가입되어 있다면 LoginUseCase 호출
-     * 3. 가입되어 있지 않다면 SignUp으로 이동
-     */
+    
     suspend fun login(accessToken: String) {
         val providerToken = ProviderToken.kakao(accessToken)
         val isSignedUp = isSignedUpUseCase(providerToken).getOrThrow()
 
         if (isSignedUp) {
-            // TODO: 온보딩 진행 여부에 따른 state 분기
+            signInUseCase(providerToken)
+                .onSuccess {
+                    val isOnboarded = isOnboardedUseCase().getOrThrow()
+                    _state.value =
+                        if (isOnboarded) LoginState.Success else LoginState.NeedToOnboard
+                }
         } else {
             _state.value = LoginState.NeedToSignUp(providerToken.accessToken)
         }
