eed6d78279517f3dc7d9161d8602160ea05d6b85
laco-dev <laco2951@gmail.com>
2022-06-26 19:44:59 +0900

    이미지 CenterCrop 추가

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/mypage/MyPageScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/mypage/MyPageScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -202,6 +203,7 @@ private fun MyPageProfile(
                 model = user.profile.imageUrl,
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
             )
         }
 
