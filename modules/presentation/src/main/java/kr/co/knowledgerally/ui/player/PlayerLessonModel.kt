4a2cccc46cd5158a2eabaf9ecde56ae96fb43551
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-03 17:24:36 +0900

    수강클래스 하위 모델이 공통 속성들을 override 하도록 수정

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/player/PlayerLessonModel.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/player/PlayerLessonModel.kt
@@ -1,32 +1,43 @@
 package kr.co.knowledgerally.ui.player
 
 import kr.co.knowledgerally.domain.model.Lesson
+import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
 sealed interface PlayerLessonModel {
+    val lessonId: String
+    val lessonTitle: String
+    val thumbnailUrl: String?
+    val coachName: String
+    val startTime: LocalDateTime
+    val runningTime: Long
 
     data class Matching(
-        val lessonId: String,
-        val lessonTitle: String,
-        val startTime: String,
-        val runningTime: String
+        override val lessonId: String,
+        override val lessonTitle: String,
+        override val thumbnailUrl: String?,
+        override val coachName: String,
+        override val startTime: LocalDateTime,
+        override val runningTime: Long
     ) : PlayerLessonModel
 
     data class Scheduled(
-        val lessonId: String,
-        val lessonTitle: String,
-        val coachName: String,
-        val coachKakaoId: String,
-        val startTime: String,
-        val runningTime: String
+        override val lessonId: String,
+        override val lessonTitle: String,
+        override val thumbnailUrl: String?,
+        override val coachName: String,
+        override val startTime: LocalDateTime,
+        override val runningTime: Long,
+        val coachKakaoId: String
     ) : PlayerLessonModel
 
     data class Completed(
-        val lessonId: String,
-        val lessonTitle: String,
-        val coachName: String,
-        val startTime: String,
-        val runningTime: String,
+        override val lessonId: String,
+        override val lessonTitle: String,
+        override val thumbnailUrl: String?,
+        override val coachName: String,
+        override val startTime: LocalDateTime,
+        override val runningTime: Long,
         val isReviewed: Boolean
     ) : PlayerLessonModel
 }
@@ -37,27 +48,31 @@ fun Lesson.toPlayerPresentation() =
             PlayerLessonModel.Matching(
                 lessonId = id.toString(),
                 lessonTitle = title,
-                startTime = startAt.toString(),
-                runningTime = ChronoUnit.HOURS.between(endAt, startAt).toString()
+                thumbnailUrl = thumbnailUrl,
+                coachName = coachName,
+                startTime = startAt,
+                runningTime = ChronoUnit.HOURS.between(endAt, startAt)
             )
         }
         Lesson.Type.Scheduled -> {
             PlayerLessonModel.Scheduled(
                 lessonId = id.toString(),
                 lessonTitle = title,
+                thumbnailUrl = thumbnailUrl,
                 coachName = coachName,
                 coachKakaoId = coachKakaoId,
-                startTime = startAt.toString(),
-                runningTime = ChronoUnit.HOURS.between(endAt, startAt).toString()
+                startTime = startAt,
+                runningTime = ChronoUnit.HOURS.between(endAt, startAt)
             )
         }
         Lesson.Type.Completed -> {
             PlayerLessonModel.Completed(
                 lessonId = id.toString(),
                 lessonTitle = title,
+                thumbnailUrl = thumbnailUrl,
                 coachName = coachName,
-                startTime = startAt.toString(),
-                runningTime = ChronoUnit.HOURS.between(endAt, startAt).toString(),
+                startTime = startAt,
+                runningTime = ChronoUnit.HOURS.between(endAt, startAt),
                 isReviewed = isReviewed
             )
         }
