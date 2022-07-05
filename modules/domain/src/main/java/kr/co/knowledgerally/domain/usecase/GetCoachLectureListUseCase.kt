8ce920a76884ca959db618d3fdd1bb70d1c6a168
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-06 05:23:18 +0900

    운영 및 수강클래스 조회 유즈케이스 수정

--- modules/domain/src/main/java/kr/co/knowledgerally/domain/usecase/GetCoachLectureListUseCase.kt
+++ modules/domain/src/main/java/kr/co/knowledgerally/domain/usecase/GetCoachLectureListUseCase.kt
@@ -1,6 +1,7 @@
 package kr.co.knowledgerally.domain.usecase
 
 import kr.co.knowledgerally.domain.model.Lecture
+import kr.co.knowledgerally.domain.model.LectureBundle
 import kr.co.knowledgerally.domain.repo.LectureRepository
 import javax.inject.Inject
 
@@ -8,5 +9,13 @@ class GetCoachLectureListUseCase @Inject constructor(
     private val lectureRepository: LectureRepository
 ) {
 
-    suspend operator fun invoke(): Result<List<Lecture>> = lectureRepository.getCoachLectures()
+    suspend operator fun invoke(): Result<LectureBundle> = runCatching {
+        val lectures = lectureRepository.getCoachLectures().getOrThrow()
+
+        LectureBundle(
+            onboardingLectures = lectures.filterIsInstance<Lecture.Onboard>(),
+            ongoingLectures = lectures.filterIsInstance<Lecture.Ongoing>(),
+            doneLectures = lectures.filterIsInstance<Lecture.Done>()
+        )
+    }
 }
