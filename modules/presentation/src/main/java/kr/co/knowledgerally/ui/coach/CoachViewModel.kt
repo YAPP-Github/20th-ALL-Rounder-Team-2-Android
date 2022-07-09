433697316f1fd4e223ac097772fe7c46e73ac818
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-09 17:39:47 +0900

    운영 클래스의 empty 여부를 User 정보에 의존하도록 수정

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/coach/CoachViewModel.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/coach/CoachViewModel.kt
@@ -4,13 +4,17 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kr.co.knowledgerally.base.BaseViewModel
 import kr.co.knowledgerally.domain.usecase.GetCoachLectureBundleUseCase
+import kr.co.knowledgerally.domain.usecase.GetUserStreamUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class CoachViewModel @Inject constructor(
+    private val getUserStreamUseCase: GetUserStreamUseCase,
     private val getCoachLectureBundleUseCase: GetCoachLectureBundleUseCase
 ) : BaseViewModel() {
 
@@ -27,18 +31,20 @@ class CoachViewModel @Inject constructor(
     private fun fetchCoachLectures() {
         _uiState.value = CoachUiState.Loading
         launch {
+            val isNotCoach = getUserStreamUseCase().map { it.coach.not() }.first()
+            if (isNotCoach) {
+                _uiState.value = CoachUiState.Empty
+                return@launch
+            }
+
             val result = getCoachLectureBundleUseCase()
             result
                 .onSuccess { lectures ->
-                    if (lectures.isEmpty) {
-                        _uiState.value = CoachUiState.Empty
-                    } else {
-                        _uiState.value = CoachUiState.Success(
-                            matchingLectures = lectures.onboardingLectures.map { it.toCoachUiState() as CoachLectureUiState.Matching },
-                            scheduledLectures = lectures.ongoingLectures.map { it.toCoachUiState() as CoachLectureUiState.Scheduled },
-                            completedLectures = lectures.doneLectures.map { it.toCoachUiState() as CoachLectureUiState.Completed },
-                        )
-                    }
+                    _uiState.value = CoachUiState.Success(
+                        matchingLectures = lectures.onboardingLectures.map { it.toCoachUiState() as CoachLectureUiState.Matching },
+                        scheduledLectures = lectures.ongoingLectures.map { it.toCoachUiState() as CoachLectureUiState.Scheduled },
+                        completedLectures = lectures.doneLectures.map { it.toCoachUiState() as CoachLectureUiState.Completed },
+                    )
                 }
                 .onFailure { _uiState.value = CoachUiState.Failure }
         }
