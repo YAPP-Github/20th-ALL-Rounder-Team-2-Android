3a9076fb5a06d2f591fdc4efd08be10ea610f6d9
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-12 00:27:03 +0900

    IllegalStateException의 message 추가

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/applicant/ApplicantActivity.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/applicant/ApplicantActivity.kt
@@ -16,7 +16,7 @@ class ApplicantActivity : BaseActivity() {
 
         val lectureInfoId: Long = intent.getLongExtra(KEY_LECTURE_INFO_ID, DEFAULT_LONG_EXTRA_VALUE)
         if (lectureInfoId == DEFAULT_LONG_EXTRA_VALUE) {
-            handleException(IllegalStateException())
+            handleException(IllegalStateException("${TAG}에서 lectureInfoId를 찾을 수 없습니다."))
             return
         }
 
