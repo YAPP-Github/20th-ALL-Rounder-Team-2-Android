2483b126ab79b4f610687881da875955765e405b
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-11 15:27:52 +0900

    Applicant 화면 삭제

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/applicant/ApplicantActivity.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/applicant/ApplicantActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.co.knowledgerally.base.BaseActivity
 import kr.co.knowledgerally.ui.theme.KnowllyTheme
@@ -12,17 +11,13 @@ import kr.co.knowledgerally.ui.theme.KnowllyTheme
 @AndroidEntryPoint
 class ApplicantActivity : BaseActivity() {
 
-    private val viewModel: ApplicantViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             KnowllyTheme {
                 ApplicantScreen(
-                    viewModel = viewModel,
-                    navigateUp = ::navigateUp,
-                    navigateToForm = { },
+                    navigateUp = ::navigateUp
                 )
             }
         }
@@ -31,8 +26,10 @@ class ApplicantActivity : BaseActivity() {
     private fun navigateUp() = finish()
 
     companion object {
-        fun getIntent(context: Context, classId: String): Intent =
+        fun getIntent(context: Context, lectureId: String): Intent =
             Intent(context, ApplicantActivity::class.java)
-                .putExtra(ApplicantViewModel.KEY_CLASS_ID, classId)
+                .putExtra(KEY_LECTURE_ID, lectureId)
+
+        private const val KEY_LECTURE_ID = "KEY_LECTURE_ID"
     }
 }
