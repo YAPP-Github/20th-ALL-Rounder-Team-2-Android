b80b4378a3d426c14ba4274c06815c42c9ec875a
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-14 02:11:56 +0900

    BaseWebViewActivity 추가

---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/base/BaseWebViewActivity.kt
@@ -0,0 +1,16 @@
+package kr.co.knowledgerally.base
+
+import kr.co.knowledgerally.bridge.WebAppInterface
+import javax.inject.Inject
+
+abstract class BaseWebViewActivity : BaseActivity() {
+
+    @Inject
+    lateinit var webAppInterface: WebAppInterface
+
+    fun navigateUp() = finish()
+
+    companion object {
+        const val DOMAIN = "knowllydev-web.hkpark.net"
+    }
+}
