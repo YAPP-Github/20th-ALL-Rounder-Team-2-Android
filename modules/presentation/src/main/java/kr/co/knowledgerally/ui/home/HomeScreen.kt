512cdefdd21700671b4ea540a0e7c306f4294bd1
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-13 21:20:44 +0900

    import 시 와일드 카드를 사용하지 않도록 수정

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/home/HomeScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/home/HomeScreen.kt
@@ -2,10 +2,18 @@ package kr.co.knowledgerally.ui.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import com.google.accompanist.web.*
+import com.google.accompanist.web.AccompanistWebChromeClient
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewNavigator
+import com.google.accompanist.web.rememberWebViewState
 import kr.co.knowledgerally.bridge.WebAppInterface
 import kr.co.knowledgerally.ui.R
 
