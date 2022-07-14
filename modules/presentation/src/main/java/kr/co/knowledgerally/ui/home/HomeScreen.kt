3aaf1d7043e81cbfa7f25b1ba28caebb2b04a87e
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-14 22:35:04 +0900

    HomeScreen을 공통 WebView 컴포넌트를 이용하도록 수정

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/home/HomeScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/home/HomeScreen.kt
@@ -1,42 +1,15 @@
 package kr.co.knowledgerally.ui.home
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import com.google.accompanist.web.AccompanistWebChromeClient
-import com.google.accompanist.web.AccompanistWebViewClient
-import com.google.accompanist.web.WebView
-import com.google.accompanist.web.rememberWebViewNavigator
-import com.google.accompanist.web.rememberWebViewState
 import kr.co.knowledgerally.bridge.WebAppInterface
+import kr.co.knowledgerally.ui.component.KnowllyWebView
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun HomeScreen(webAppInterface: WebAppInterface) {
-    val context = LocalContext.current
-
-    var url by remember { mutableStateOf("https://google.com") }
-    val webViewState = rememberWebViewState(url = url)
-    val navigator = rememberWebViewNavigator()
-
-    WebView(
-        state = webViewState,
-        modifier = Modifier.fillMaxSize(),
-        navigator = navigator,
-        onCreated = {
-            it.settings.javaScriptEnabled = true
-            it.addJavascriptInterface(
-                webAppInterface,
-                "Android"
-            )
-        },
-        client = AccompanistWebViewClient(),
-        chromeClient = AccompanistWebChromeClient()
+    KnowllyWebView(
+        webAppInterface = webAppInterface,
+        initUrl = "http://knowllydev-web.hkpark.net"
     )
 }
