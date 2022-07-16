847f5bb03b2e2cd6fc4bb90f6d6a93898e9ce234
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-16 17:00:44 +0900

    프로필 화면 상단바 편집하기 버튼 추가

--- modules/presentation/src/main/java/kr/co/knowledgerally/ui/user/UserScreen.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/user/UserScreen.kt
@@ -1,13 +1,18 @@
 package kr.co.knowledgerally.ui.user
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kr.co.knowledgerally.bridge.BridgeDelegate
 import kr.co.knowledgerally.bridge.WebViewState
 import kr.co.knowledgerally.ui.component.KnowllyTopAppBar
 import kr.co.knowledgerally.ui.component.KnowllyWebView
+import kr.co.knowledgerally.ui.theme.KnowllyTheme
 
 @Composable
 fun UserScreen(
@@ -18,7 +23,21 @@ fun UserScreen(
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        KnowllyTopAppBar(onNavigationClick = navigateUp)
+        KnowllyTopAppBar(
+            onNavigationClick = navigateUp,
+            actions = {
+                TextButton(
+                    onClick = { /* 프로필 편집 화면 이동 */ },
+                    contentPadding = PaddingValues(horizontal = 12.dp, vertical = 0.dp)
+                ) {
+                    Text(
+                        text = "편집하기",
+                        color = KnowllyTheme.colors.primaryDark,
+                        style = KnowllyTheme.typography.subtitle2
+                    )
+                }
+            }
+        )
 
         KnowllyWebView(
             state = state,
