fe2645ce3f21e6d06007e5e36ee271471b1724b4
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-16 16:44:06 +0900

    유저 프로필 웹뷰 화면 리팩토링

---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/user/UserViewModel.kt
@@ -0,0 +1,36 @@
+package kr.co.knowledgerally.ui.user
+
+import androidx.lifecycle.SavedStateHandle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kr.co.knowledgerally.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UserViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel() {
+
+    private val userId: Long = savedStateHandle.get<Long>(KEY_USER_ID)!!
+
+    private val _url =
+        MutableStateFlow("${BASE_URL}/profile/$userId")
+    val url = _url.asStateFlow()
+
+    private val _isRefresh = MutableStateFlow(false)
+    val isRefresh = _isRefresh.asStateFlow()
+
+    fun refresh() {
+        _isRefresh.value = true
+    }
+
+    fun onRefresh() {
+        _isRefresh.value = false
+    }
+
+    companion object {
+        private const val BASE_URL = "http://knowllydev-web.hkpark.net"
+        const val KEY_USER_ID = "KEY_USER_ID"
+    }
+}
