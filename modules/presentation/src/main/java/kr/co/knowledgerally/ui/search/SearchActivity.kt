7e341981234854f5c40c24e5c5d2793aa9af9d79
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-16 17:15:48 +0900

    검색 웹뷰 화면 추가

---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/search/SearchActivity.kt
@@ -0,0 +1,63 @@
+package kr.co.knowledgerally.ui.search
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import dagger.hilt.android.AndroidEntryPoint
+import kr.co.knowledgerally.base.BaseWebViewActivity
+import kr.co.knowledgerally.bridge.BridgeRequest
+import kr.co.knowledgerally.bridge.BridgeResponse
+import kr.co.knowledgerally.bridge.rememberWebViewState
+import kr.co.knowledgerally.ui.lecture.LectureActivity
+import kr.co.knowledgerally.ui.theme.KnowllyTheme
+
+@AndroidEntryPoint
+class SearchActivity : BaseWebViewActivity() {
+
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            KnowllyTheme {
+                val url by viewModel.url.collectAsState()
+                val isRefresh by viewModel.isRefresh.collectAsState()
+                val webViewState = rememberWebViewState(url = url)
+
+                SearchScreen(
+                    state = webViewState,
+                    delegate = this,
+                    navigateUp = ::navigateUp
+                )
+
+                LaunchedEffect(isRefresh) {
+                    if (isRefresh) {
+                        webViewState.request(BridgeRequest.Refresh)
+                        viewModel.onRefresh()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onBridgeResponse(response: BridgeResponse) = when (response) {
+        is BridgeResponse.NavigateToLecture -> {
+            val intent = LectureActivity.getIntent(this, response.lectureInfoId)
+            startActivity(intent)
+        }
+        else -> {}
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            val intent = Intent(context, SearchActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
---
+++ modules/presentation/src/main/java/kr/co/knowledgerally/ui/search/SearchScreen.kt
@@ -0,0 +1,28 @@
+package kr.co.knowledgerally.ui.search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kr.co.knowledgerally.bridge.BridgeDelegate
+import kr.co.knowledgerally.bridge.WebViewState
+import kr.co.knowledgerally.ui.component.KnowllyTopAppBar
+import kr.co.knowledgerally.ui.component.KnowllyWebView
+
+@Composable
+fun SearchScreen(
+    state: WebViewState,
+    delegate: BridgeDelegate,
+    navigateUp: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        KnowllyTopAppBar(onNavigationClick = navigateUp)
+
+        KnowllyWebView(
+            state = state,
+            delegate = delegate,
+        )
+    }
+}
