23b08c8e57e7eb399eb788bd5745d4041bcb3fe6
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-20 23:19:48 +0900

    클래스 카테고리 화면 브릿지 구현

--- modules/presentation/src/main/java/kr/co/knowledgerally/bridge/BridgeResponse.kt
+++ modules/presentation/src/main/java/kr/co/knowledgerally/bridge/BridgeResponse.kt
@@ -6,7 +6,9 @@ import androidx.compose.runtime.Stable
 sealed interface BridgeResponse {
 
     data class NavigateToLecture(val lectureInfoId: Long) : BridgeResponse
-    
+
+    data class NavigateToCategory(val category: String) : BridgeResponse
+
     object NavigateToSearch : BridgeResponse
 
     object NavigateUp : BridgeResponse
